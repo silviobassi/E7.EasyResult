b556aaa84d30695b0e7bdd0dd364668f3245e4f3
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-01-19 16:05:39 -0300

    setting: sending project

---
+++ E7.Result/Errors/AppError.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+
+namespace E7.Result.Errors;
+
+/// <summary>
+/// Represents an abstract base class for application errors.
+/// </summary>
+/// <param name="detail">The detail message of the error.</param>
+/// <param name="errorType">The type of the error.</param>
+/// <param name="errorCodeName">The name of the error code.</param>
+public abstract class AppError(string detail, ErrorType errorType, string errorCodeName)
+{
+    /// <summary>
+    /// Gets the detail message of the error.
+    /// </summary>
+    protected string Detail { get; } = detail;
+
+    /// <summary>
+    /// Gets the type of the error.
+    /// </summary>
+    public ErrorType ErrorType { get; } = errorType;
+
+    /// <summary>
+    /// Gets the name of the error code.
+    /// </summary>
+    public string ErrorCodeName { get; } = errorCodeName;
+
+    /// <summary>
+    /// Gets the list of error messages.
+    /// </summary>
+    /// <returns>A list of error messages.</returns>
+    public abstract List<string?> GetErrorsMessage();
+
+    /// <summary>
+    /// Gets the HTTP status code associated with the error.
+    /// </summary>
+    /// <returns>The HTTP status code.</returns>
+    public abstract HttpStatusCode GetHttpStatusCode();
+}
---
+++ E7.Result/ResultHandler.cs
@@ -0,0 +1,23 @@
+﻿using E7.Result.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E7.Result;
+
+/// <summary>
+/// Handles matching logic for results, allowing separation of concerns.
+/// </summary>
+internal static class ResultHandler
+{
+    /// <summary>
+    /// Matches the result to a corresponding action for success or failure.
+    /// </summary>
+    /// <param name="result">The result to match.</param>
+    /// <param name="onSuccess">The action to execute on success.</param>
+    /// <param name="onFailure">The action to execute on failure.</param>
+    /// <returns>An <see cref="IActionResult"/> representing the result of the match.</returns>
+    public static IActionResult Match(
+        this Result result, Func<IActionResult> onSuccess,
+        Func<AppError, IActionResult> onFailure
+    )
+        => result.IsSuccess ? onSuccess() : onFailure(result.Error!);
+}
