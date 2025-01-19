426648b4557a587cb7a19a8d243bd1ba630c0df1
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-01-19 17:39:44 -0300

    test: creating result handler test of the result

--- src/E7.Result/ResultHandler.cs
+++ src/E7.Result/ResultHandler.cs
@@ -1,12 +1,11 @@
 ﻿using E7.Result.Errors;
-using Microsoft.AspNetCore.Mvc;
 
 namespace E7.Result;
 
 /// <summary>
 /// Handles matching logic for results, allowing separation of concerns.
 /// </summary>
-internal static class ResultHandler
+public static class ResultHandler
 {
     /// <summary>
     /// Matches the result to a corresponding action for success or failure.
@@ -15,9 +14,6 @@ internal static class ResultHandler
     /// <param name="onSuccess">The action to execute on success.</param>
     /// <param name="onFailure">The action to execute on failure.</param>
     /// <returns>An <see cref="IActionResult"/> representing the result of the match.</returns>
-    public static IActionResult Match(
-        this Result result, Func<IActionResult> onSuccess,
-        Func<AppError, IActionResult> onFailure
-    )
+    public static T Match<T>(this Result result, Func<T> onSuccess, Func<AppError, T> onFailure)
         => result.IsSuccess ? onSuccess() : onFailure(result.Error!);
 }
---
+++ tests/E7.Result.Tests/ResultHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using E7.Result.Errors;
+using FluentAssertions;
+
+namespace E7.Result.Tests;
+
+public class ResultHandlerTests
+{
+    [Fact]
+    public void Match_ShouldReturnOnSuccess_WhenResultIsSuccess()
+    {
+        var result = Result.Success();
+
+        var actionResult = result.Match<object>(
+            () => result,
+            _ => result.Error!
+        );
+
+        actionResult.Should().Be(result);
+        actionResult.Should().NotBe(result.Error);
+    }
+
+
+    [Fact]
+    public void Match_ShouldReturnOnFailure_WhenResultIsFailure()
+    {
+        var error = new TestAppError("Error", ErrorType.ValidationRule, nameof(TestAppError));
+        var result = Result.Failure(error);
+
+        var actionResult = result.Match<object>(
+            () => result,
+            _ => result.Error!
+        );
+
+        actionResult.Should().Be(result.Error);
+        actionResult.Should().NotBe(result);
+    }
+}
