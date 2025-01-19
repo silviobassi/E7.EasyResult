9d3c023a993cb2faa1e0e6b39a15be359ff039e4
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-01-19 17:07:34 -0300

    refactor: creating class for to remove duplication

--- tests/E7.Result.Tests/Errors/AppErrorTests.cs
+++ tests/E7.Result.Tests/Errors/AppErrorTests.cs
@@ -6,22 +6,13 @@ namespace E7.Result.Tests.Errors;
 
 public class AppErrorTests
 {
-    private class TestAppError(string detail, ErrorType errorType, string errorCodeName)
-        : AppError(detail, errorType, errorCodeName)
-    {
-        public override List<string?> GetErrorsMessage() => [Detail];
-        
-        public override HttpStatusCode GetHttpStatusCode() => HttpStatusCode.BadRequest;
-        
-    }
-
     [Fact]
     public void GetErrorsMessage_ShouldReturnListOfErrorMessages()
     {
         var error = new TestAppError("Detail", ErrorType.ValidationRule, "ErrorCode");
-        
+
         var result = error.GetErrorsMessage();
-        
+
         result.Should().NotBeNull();
         result.Should().HaveCount(1);
         result.Should().Contain("Detail");
@@ -31,10 +22,10 @@ public class AppErrorTests
     public void GetHttpStatusCode_ShouldReturnBadRequest()
     {
         var error = new TestAppError("Detail", ErrorType.BadRequestRule, nameof(TestAppError));
-        
+
         var resultError = error.GetHttpStatusCode();
         var resultErrorType = error.ErrorType;
-        
+
         resultError.Should().Be(HttpStatusCode.BadRequest);
         resultErrorType.Should().Be(ErrorType.BadRequestRule);
     }
